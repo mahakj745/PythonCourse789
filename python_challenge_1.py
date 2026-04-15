e1d2b4ee75e59bb145513d01c740d6fdbca7a4e0
Mahak Jain <mahakj745@gmail.com>
2026-04-14 20:14:37 -0700

    completed python challenge 1

---
+++ python_challenge_1.py
@@ -0,0 +1,27 @@
+#print("Hi, this is my first python code")
+#print("I am learning python programming language")
+#print("I am enjoying learning python")
+#start
+#print ("""Your Learning Path:
+ #      \t-Python Basics
+  #     \t-Data Engineering
+   #    \t-AI""")
+
+
+#website = "datawithbaraa.com"
+#print ("info@datawithbaraa.com" )
+#print ("support@datawithbaraa.com")
+#print(website) 
+
+
+Age = 31
+Height = 5.1
+Name = "Mahak"
+is_student = True
+Empty = None
+print(Age, type(Age), len(str(Age)))
+print(Height, type(Height), len(str(Height)))
+print(Name, type(Name), len(str(Name)))
+print(is_student, type(is_student), len(str(is_student)))
+print(Empty, type(Empty), len(str(Empty)))
+#end of 1st python challenge
