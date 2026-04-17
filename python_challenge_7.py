3e9d75ea707ceb145a48520d799093586fae2271
Mahak Jain <mahakj745@gmail.com>
2026-04-16 19:46:01 -0700

    python_challenge_7

---
+++ python_challenge_7.py
@@ -0,0 +1,9 @@
+
+
+phone1 = "+48-176-12345"
+phone2 = "48-654-16548"
+phone3 = "0048-654-16548"
+
+print(phone1[phone1.find("-")+1:])
+print(phone2[phone2.find("-")+1:])
+print(phone3[phone3.find("-")+1:])
