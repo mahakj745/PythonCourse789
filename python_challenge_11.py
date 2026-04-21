b8ca9be0729104f12df1e12ec07a73356937c75d
Mahak Jain <mahakj745@gmail.com>
2026-04-20 19:19:28 -0700

    python_challenge_11

---
+++ python_challenge_11.py
@@ -0,0 +1,15 @@
+#print (3 > 1 and 5 < 1)
+
+#print (3 > 1 or 5 < 1)
+# check if system is under pressure
+
+cpu_usage = 70
+memory_usage = 95
+print (cpu_usage > 90 or memory_usage >90)
+
+#check user credentials before login
+
+email = True
+password = False
+
+print (email and password)
